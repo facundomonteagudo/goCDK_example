0d3a858421639fc6c4555f412eae2d489c7e1fd8
Facundo Monteagudo <facundo_monteagudo@outlook.es>
2024-05-05 19:45:08 -0300

    initial commit

---
+++ lambda/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Username string `json:"username"`
+}
+
+func HandleRequest(event MyEvent) (string, error) {
+	if event.Username == "" {
+		return "", fmt.Errorf("username cannot be empty")
+	}
+
+	return fmt.Sprintf("Successfully called by - %s", event.Username), nil
+}
+
+func main() {
+	// myApp := app.NewApp()
+	lambda.Start(HandleRequest)
+}
